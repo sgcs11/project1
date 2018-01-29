eeb2812b124611e87368923df41d1d271189fbac
Joa Chang Hwa <joachy@naver.com>
2018-01-29 16:40:06 +0900

    made simple menu

--- app/src/main/java/com/example/joachanghwa/svoprj/MainActivity.java
+++ app/src/main/java/com/example/joachanghwa/svoprj/MainActivity.java
@@ -1,7 +1,9 @@
 package com.example.joachanghwa.svoprj;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +11,26 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void onClicked1(View v){
+        Intent intent=new Intent(getApplicationContext(),quicksolve.class);
+        startActivity(intent);
+
+    }
+    public void onClicked2(View v){
+        Intent intent=new Intent(getApplicationContext(),quickteam.class);
+        startActivity(intent);
+
+    }
+    public void onClicked3(View v){
+        Intent intent=new Intent(getApplicationContext(),svogame.class);
+        startActivity(intent);
+
+    }
+    public void onClicked4(View v){
+        Intent intent=new Intent(getApplicationContext(),directinput.class);
+        startActivity(intent);
+
     }
 }
---
+++ app/src/main/java/com/example/joachanghwa/svoprj/svogame.java
@@ -0,0 +1,14 @@
+package com.example.joachanghwa.svoprj;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+public class svogame extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_svogame);
+    }
+
+}
